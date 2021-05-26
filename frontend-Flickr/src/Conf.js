db976fe52d1407ecd87aae63b67389226ed74ced
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-05-26 21:12:56 +0200

    Configuration

--- frontend-Flickr/src/Conf.js
+++ frontend-Flickr/src/Conf.js
@@ -3,5 +3,9 @@ export default {
   apiURL: 'http://127.0.0.1:8000',
   localURL: 'http://localhost:5000/photoData',
   LocalNoPath: 'http://localhost:5000',
-  backURL: 'http://fotone.me:8000/',
+  backURL: 'https://fotone.me/api/',
+  productionURL: 'http://localhost:8000',
 };
+
+// eslint-disable-next-line eol-last
+// GALLERY_URL_GET
