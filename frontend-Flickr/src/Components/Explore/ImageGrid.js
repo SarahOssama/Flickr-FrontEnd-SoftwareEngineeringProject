37a71b8f7894c5618940134acca65717714fd65b
Mariam <mariamessammohamed98@gmail.com>
2021-05-28 01:49:53 +0200

    Update ImageGrid.js

--- frontend-Flickr/src/Components/Explore/ImageGrid.js
+++ frontend-Flickr/src/Components/Explore/ImageGrid.js
@@ -3,7 +3,7 @@ import './ImageGrid.css';
 import Photo from '../HomePage/Items/Photo';
 
 const ImageGrid = ({ photoData }) => (
-  <div data-testid="recentPhotosTest" className="imageContainer">
+  <div className="imageContainer">
     {photoData.map((pDetails) => (<Photo key={pDetails.id} pCard={pDetails} />
     ))}
   </div>
