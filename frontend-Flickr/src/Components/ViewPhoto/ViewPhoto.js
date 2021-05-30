92c6ba4a4e61018ea12b64857377d6c3281a46ed
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-05-30 07:39:44 +0200

    View Photo Share

--- frontend-Flickr/src/Components/ViewPhoto/ViewPhoto.js
+++ frontend-Flickr/src/Components/ViewPhoto/ViewPhoto.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ViewPhoto.css';
 
-const ViewPhoto = () => {
-  const Photo = 'ViewPhoto';
-  return (
-    <div>
-      <h1>{Photo}</h1>
+import UpperView from './ViewPhotoItems/UpperView';
+
+const ViewPhoto = () => (
+  <div className=" viewPhotoPage">
+    <div className="viewPhotoUpperComp">
+      <UpperView />
     </div>
-  );
-};
+
+    <div className=".container fluid-centered viewSubPhotoInfo">
+      {/* <div className=" centered-content   subPhotoContainer"> */}
+      <div className="col-xl-12 rowViewPhotoInfo">
+        <div className="col-lg-7 viewSubViewLeft">
+          <h2>Left View</h2>
+        </div>
+        <div className="col-lg-5 viewSubViewPhotoRight">
+          <h2>Right View</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  // </div>
+);
 
 export default ViewPhoto;
