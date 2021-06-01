2742f3a0408f9faf112aae657e3fc604e9577d43
Ziyad <17732483+ziyadss@users.noreply.github.com>
2021-06-01 07:27:04 +0200

    updated script to be like one used in backend

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,17 @@ docker-compose push
 
 #Sets file modification times to last commit they were changed.
 #Done to minimize files sent using rsync.
-python3 git-restore-mtime
+#python3 git-restore-mtime
 
 #Remove dockerfiles
 rm frontend-Flickr/Dockerfile*
 
+#ssh ubuntu@$EC2_INSTANCE "sudo rm -r  fotone/flickr-backend/*/migrations"
+
 #Sending of project files to be used as volumes,
 #allowing static files to be changed without reloading containers
-rsync -e "ssh -o StrictHostKeyChecking=no" -au frontend-Flickr upload/docker-compose.yml ubuntu@$EC2_INSTANCE:fotone
+#rsync -e "ssh -o StrictHostKeyChecking=no" -au frontend-Flickr upload/docker-compose.yml ubuntu@$EC2_INSTANCE:fotone
+scp -o StrictHostKeyChecking=no -r frontend-Flickr upload/docker-compose.yml ubuntu@$EC2_INSTANCE:fotone
 
 #From instance, pulls updated images and reloads the containers if they were changed.
 ssh ubuntu@$EC2_INSTANCE "cd fotone; sudo docker-compose pull; sudo docker-compose up --build -d"
