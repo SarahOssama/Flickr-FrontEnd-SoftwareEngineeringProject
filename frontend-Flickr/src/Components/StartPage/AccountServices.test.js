84d380441d5b5517dc7f87127568e27955877065
mennat allah nawar <55787939+mennatallah-nawar@users.noreply.github.com>
2021-06-07 23:54:09 +0200

    Fix testing requests

--- frontend-Flickr/src/Components/StartPage/AccountServices.test.js
+++ frontend-Flickr/src/Components/StartPage/AccountServices.test.js
@@ -1,74 +1,72 @@
+/* eslint-disable max-len */
 import AccountServices from './AccountServices';
-// test by passing all SignUp data
+
+// const AccountServices = require('./AccountServices');
+
 const data = {
   first_name: 'sara',
   last_name: 'ossama',
   age: '22',
-  email: 'menna.nawar99@gmail.com',
+  email: 'talatawf@gmail.com',
   password: 'Saraossama11111',
 };
-test('good Local SignUp request', () => {
-  expect(AccountServices.addUser(data)).toBeTruthy();
+test('good Local SignUp request', async () => {
+  expect(await AccountServices.addUser(data)).toBeTruthy();
 });
 
-test('good Back SignUp request', () => {
-  expect(AccountServices.backaddUser(data)).toBeTruthy();
+test('good Back SignUp request', async () => {
+  expect(await AccountServices.backaddUser(data)).toBeTruthy();
 });
 
-// test by passing passing correct email
 const emailAddress = {
-  email: 'menna.nawar@gmail.com',
+  email: 'menna.nawar99@gmail.com',
 };
-test('good local SendVerificationLink request', () => {
-  expect(AccountServices.CompleteSignup(emailAddress)).toBeTruthy();
+test('good local SendVerificationLink request', async () => {
+  expect(await AccountServices.CompleteSignup(emailAddress)).toBe();
 });
 
-test('good Back SendVerificationLink request', () => {
-  expect(AccountServices.backCompleteSignup(emailAddress.email)).toBeTruthy();
+test('good Back SendVerificationLink request', async () => {
+  expect(await AccountServices.backCompleteSignup(emailAddress.email)).toEqual(201);
 });
 
-// test by passing all Login data
 const info = {
-  email: 'menna.nawar@gmail.com',
+  email: 'menna.nawar99@gmail.com',
   password: 'Mennakamel123456',
 };
 
-test('good Local Login request', () => {
-  expect(AccountServices.LoginUser(info)).toBeTruthy();
+test('good Local Login request', async () => {
+  expect(await AccountServices.LoginUser(info)).toBeTruthy();
 });
 
-test('good Back Login request', () => {
-  expect(AccountServices.backLoginUser(info)).toBeTruthy();
+test('good Back Login request', async () => {
+  expect(await AccountServices.backLoginUser(info)).toBeTruthy();
 });
 
-// test by passing correct email
 const email = {
-  email: 'menna.nawar@gmail.com',
+  email: 'menna.nawar99@gmail.com',
 };
-test('good Local EmailToresetPassword request', () => {
-  expect(AccountServices.emailToResetPassword(email)).toBeTruthy();
+test('good Local EmailToresetPassword request', async () => {
+  expect(await AccountServices.emailToResetPassword(email)).toBeTruthy();
 });
 
-test('good Back EmailToresetPassword request', () => {
-  expect(AccountServices.backemailToResetPassword(email)).toBeTruthy();
+test('good Back EmailToresetPassword request', async () => {
+  expect(await AccountServices.backemailToResetPassword(email)).toBeTruthy();
 });
 
-// test by passing correct email
 const ResendemailToReset = {
-  email: 'menna.nawar@gmail.com',
+  email: 'menna.nawar99@gmail.com',
 };
-test('good Local ResendEmailToReset request', () => {
-  expect(AccountServices.ResendEmailsToReset(ResendemailToReset)).toBeTruthy();
+test('good Local ResendEmailToReset request', async () => {
+  expect(await AccountServices.ResendEmailsToReset(ResendemailToReset)).toBe();
 });
 
-test('good Back ResendEmailToReset request', () => {
-  expect(AccountServices.backResendEmailsToReset(ResendemailToReset)).toBeTruthy();
+test('good Back ResendEmailToReset request', async () => {
+  expect(AccountServices.backResendEmailsToReset('menna.nawar99@gmail.com')).resolves.toEqual(200);
 });
 
-// test by passing correct new password
 const newPassword = {
   password: '11111111111111111',
 };
-test('good set new password request', () => {
-  expect(AccountServices.addNewPassword(newPassword)).toBeTruthy();
+test('good set new password request', async () => {
+  expect(await AccountServices.addNewPassword(newPassword)).toBeTruthy();
 });
